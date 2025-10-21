999b821172fb631668cea720950fa0f046f9c245
Prashant V Gaikar <1729prashant@gmail.com>
2025-10-21 20:01:02 +0530

    chunking, chunking overlap

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import lib.semantic_search as ll
 
+
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
@@ -21,6 +22,12 @@ def main():
     search_embeddings = subparsers.add_parser("search", help="term you want to search for")
     search_embeddings.add_argument("query", type=str, help="query text")
     search_embeddings.add_argument("--limit", type=int, default=5, help="Number of results to return")
+
+
+    chunking = subparsers.add_parser("chunk", help="chunk texts")
+    chunking.add_argument("query", type=str, help="text to chunk")
+    chunking.add_argument("--chunk-size", type=int, default=200, help="chunking size")
+    chunking.add_argument("--overlap", type=int, default=0, help="overlap amount")
 
     args = parser.parse_args()
     match args.command:
@@ -47,10 +54,30 @@ def main():
             llminilm_semantic_load.load_or_create_embeddings(documents)
 
             search_results = llminilm_semantic_load.search(args.query,args.limit)
-            i=1
+            
             for i, result in enumerate(search_results, start=1):
                 print(f"{i}. {result['title']} (score: {result['score']:.4f})")
                 print(f"   {result['description']}\n")
+
+        case "chunk":
+            words = args.query.split()
+            chunk_size = args.chunk_size
+            overlap = args.overlap
+            chunked_text = []
+
+            step = chunk_size - overlap
+            if step <= 0:
+                print("Error: overlap must be smaller than chunk size.")
+                return
+
+            for i in range(0, len(words), step):
+                group = words[i:i + chunk_size]
+                chunk = " ".join(group)
+                chunked_text.append(chunk)
+
+            print(f"Chunking {len(args.query)} characters")
+            for i, chunk_line in enumerate(chunked_text, start=1):
+                print(f"{i}. {chunk_line}")
 
         case _:
             parser.print_help()
