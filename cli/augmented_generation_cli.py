0fd5783fcf5cd0c19e7fb7f62cf2f46e8998ef82
Prashant V Gaikar <1729prashant@gmail.com>
2025-10-31 13:44:26 +0530

    LLM citations

--- cli/augmented_generation_cli.py
+++ cli/augmented_generation_cli.py
@@ -28,7 +28,6 @@ Provide a comprehensive answer that addresses the query:"""
 
 
 def rag_command(query: str, limit: int, k = 60) -> None:
-
     movies = load_movies()
     searcher = HybridSearch(movies)
     results = searcher.rrf_search(query, k, limit)
@@ -73,6 +72,45 @@ def rag_summarize_command(query: str, limit: int, k = 60) -> None:
     )
     print(response.text)
 
+
+def gemini_citations_prompt(query: str, results) -> str:
+    return  f"""Answer the question or provide information based on the provided documents.
+
+This should be tailored to Hoopla users. Hoopla is a movie streaming service.
+
+If not enough information is available to give a good answer, say so but give as good of an answer as you can while citing the sources you have.
+
+Query: {query}
+
+Documents:
+{results}
+
+Instructions:
+- Provide a comprehensive answer that addresses the query
+- Cite sources using [1], [2], etc. format when referencing information
+- If sources disagree, mention the different viewpoints
+- If the answer isn't in the documents, say "I don't have enough information"
+- Be direct and informative
+
+Answer:"""
+
+def rag_citations_command(query: str, limit: int, k = 60) -> None:
+    movies = load_movies()
+    searcher = HybridSearch(movies)
+    results = searcher.rrf_search(query, k, limit)
+
+    print(f"Search Results:")
+    for i, result in enumerate(results, start=1):
+        print(f"  - {result['title']}")
+
+    print(f"\nLLM Answer:")
+    response = client.models.generate_content(
+        model='gemini-2.0-flash', 
+        contents=gemini_citations_prompt(query, results)
+    )
+    print(response.text)
+
+
 def main():
     parser = argparse.ArgumentParser(description="Retrieval Augmented Generation CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
@@ -83,6 +121,10 @@ def main():
     rag_summarize_parser = subparsers.add_parser("summarize", help="Perform Summary RAG (search + generate answer)")
     rag_summarize_parser.add_argument("query", type=str, help="Search query for RAG")
     rag_summarize_parser.add_argument("--limit", type=int, default=5, help="Number of results to return")
+
+    rag_citations_parser = subparsers.add_parser("citations", help="Perform Citations RAG (search + generate answer)")
+    rag_citations_parser.add_argument("query", type=str, help="Search query for RAG")
+    rag_citations_parser.add_argument("--limit", type=int, default=5, help="Number of results to return")
 
 
     args = parser.parse_args()
@@ -95,6 +137,9 @@ def main():
         case "summarize":
             rag_summarize_command(args.query, args.limit)
 
+        case "citations":
+            rag_citations_command(args.query, args.limit)
+            
         case _:
             parser.print_help()
 
